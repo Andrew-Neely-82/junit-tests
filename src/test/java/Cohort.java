9f6d10f7f46d44bd43cf3274196f15073598c326
Andrew-Neely-82 <114946953+Andrew-Neely-82@users.noreply.github.com>
2023-01-20 15:43:19 -0600

    spacing

--- src/test/java/Cohort.java
+++ src/test/java/Cohort.java
@@ -2,33 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cohort {
-    private List<Student> students;
+  private List<Student> students;
 
-    public Cohort() {
-        students = new ArrayList<>();
-    }
+  public Cohort() {
+    students = new ArrayList<>();
+  }
 
-    public void addStudent(Student student) {
-        students.add(student);
-    }
+  public void addStudent(Student student) {
+    students.add(student);
+  }
 
-    public List<Student> getStudents() {
-        return students;
-    }
+  public List<Student> getStudents() {
+    return students;
+  }
 
-    public double getCohortAverage() {
-        double sum = 0;
-        for (Student student : students) {
-            sum += student.getGradeAverage();
-        }
-        return sum / students.size();
+  public double getCohortAverage() {
+    double sum = 0;
+    for (Student student : students) {
+      sum += student.getGradeAverage();
     }
-    public Student findStudentById(long id) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                return student;
-            }
-        }
-        return null;
+    return sum / students.size();
+  }
+  public Student findStudentById(long id) {
+    for (Student student : students) {
+      if (student.getId() == id) {
+        return student;
+      }
     }
+    return null;
+  }
 }
--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,37 +1,37 @@
 import java.util.ArrayList;
 
 public class Student {
-    private long id;
-    private String name;
-    private ArrayList<Integer> grades;
+  private long id;
+  private String name;
+  private ArrayList<Integer> grades;
 
-    public Student(long id, String name) {
-        this.id = id;
-        this.name = name;
-        this.grades = new ArrayList<>();
-    }
+  public Student(long id, String name) {
+    this.id = id;
+    this.name = name;
+    this.grades = new ArrayList<>();
+  }
 
-    public long getId() {
-        return id;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void addGrade(int grade) {
-        grades.add(grade);
-    }
+  public void addGrade(int grade) {
+    grades.add(grade);
+  }
 
-    public ArrayList<Integer> getGrades() {
-        return grades;
-    }
+  public ArrayList<Integer> getGrades() {
+    return grades;
+  }
 
-    public double getGradeAverage() {
-        int sum = 0;
-        for (int grade : grades) {
-            sum += grade;
-        }
-        return (double) sum / grades.size();
+  public double getGradeAverage() {
+    int sum = 0;
+    for (int grade : grades) {
+      sum += grade;
     }
+    return (double) sum / grades.size();
+  }
 }
